e3fa0d4d745830afbc831053b85891ae1d7c6128
wardler-ah <78051126+wardler-ah@users.noreply.github.com>
2022-01-06 21:19:25 -0500

    Refactoring

--- ActiveDirectoryManager.cs
+++ ActiveDirectoryManager.cs
@@ -1,55 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.DirectoryServices.AccountManagement;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.DirectoryServices.AccountManagement;
 
-namespace ADCC
+namespace ADCC;
+
+public class ActiveDirectoryManager
 {
-    public class ActiveDirectoryManager
+    private PrincipalContext? _currentContext;
+    private UserPrincipal? _userOfInterest;
+
+    public ActiveDirectoryManager(PrincipalContext context)
     {
-        private PrincipalContext _currentContext;
-        private UserPrincipal _userOfInterest;
-        public ActiveDirectoryManager(PrincipalContext context)
-        {
-            _currentContext = context;
-            _userOfInterest = null;
-        }
-        public void SetUserOfInterestByIdentity(string searchCriteria)
-        {
-            if (_userOfInterest != null) { _userOfInterest.Dispose(); }
-            _userOfInterest = UserPrincipal.FindByIdentity(_currentContext, searchCriteria);
-        }
-        public bool UnlockUser()
-        {
-            if (_userOfInterest == null || !_userOfInterest.IsAccountLockedOut()) return false;
-            _userOfInterest.UnlockAccount();
-            return true;
-        }
-        public bool SetPassword(string password)
-        {
-            if (_userOfInterest == null) return false;
-            _userOfInterest.SetPassword(password);
-            return true;
-        }
+        _currentContext = context;
+        _userOfInterest = null;
+    }
 
-        public string GetDistinguishedName()
-        {
-            if (_userOfInterest == null) return null;
-            string distinguishedName = _userOfInterest.DistinguishedName;
-            return distinguishedName;
+    public void SetUserOfInterestByIdentity(string searchCriteria)
+    {
+        _userOfInterest?.Dispose();
+        _userOfInterest = UserPrincipal.FindByIdentity(_currentContext, searchCriteria);
+    }
 
-        }
-        public void SetContext(PrincipalContext context)
-        {
-            if (_currentContext != null) { _currentContext.Dispose(); }
-            _currentContext = context;
+    public bool UnlockUser()
+    {
+        if (_userOfInterest == null || !_userOfInterest.IsAccountLockedOut()) return false;
+        _userOfInterest.UnlockAccount();
+        return true;
+    }
 
-            if (_userOfInterest != null) { _userOfInterest.Dispose(); }
-            _userOfInterest = null;
-        }
+    public bool SetPassword(string password)
+    {
+        _userOfInterest?.SetPassword(password);
+        return true;
+    }
+
+    public string? GetDistinguishedName()
+    {
+        return _userOfInterest?.DistinguishedName;
+    }
+
+    public void SetContext(PrincipalContext context)
+    {
+        _currentContext?.Dispose();
+
+        _currentContext = context;
+
+        _userOfInterest?.Dispose();
+
+        _userOfInterest = null;
     }
 }
-    
